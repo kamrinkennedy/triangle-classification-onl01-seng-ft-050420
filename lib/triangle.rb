53eb09b26d64e2240160af8ec2d65f272a893ce6
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-20 17:00:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,20 @@
 class Triangle
-  attr_accessor :length_one, :length_two, :length_three
+  attr_accessor :a, :b, :c
   
   def initialize(length_one, length_two, length_three) 
-    @length_one = length_one
-    @length_two = length_two
-    @length_three = length_three
+    @a = length_one
+    @b = length_two
+    @c = length_three
+    @length_array = [length_one, length_two, length_three].sort
+  end
+  
+  def valid?
+    
   end
   
   def kind 
+    if 
+    end
   end
   
   class TriangleError < StandardError
