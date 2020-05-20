515f0c9513643478faeb55dd5f9354f62018cad8
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-20 17:08:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,26 +7,26 @@ class Triangle
     @c = c
   end
   
-  def valid?
-    if (@a < 0 || @b < 0 || @c < 0) 
-      return false 
-    elsif (@a + @b <= @c || @a + @c <= @b || @b + @c <= @a )
-      return false 
-    else 
-      return true
-    end
-  end
+  # def valid?
+  #   if (@a < 0 || @b < 0 || @c < 0) 
+  #     return false 
+  #   elsif (@a + @b <= @c || @a + @c <= @b || @b + @c <= @a )
+  #     return false 
+  #   else 
+  #     return true
+  #   end
+  # end
   
   def kind 
     if 
     end
   end
   
-  # class TriangleError < StandardError
-  #   def message 
+  class TriangleError < StandardError
+    def message 
       
-  #   end
-  # end
+    end
+  end
   
   
 end
